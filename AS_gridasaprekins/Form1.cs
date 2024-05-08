c8c55be8a8ca878eb31afaf583ee7b2dfb002a26
Adrians Stepe <as3616@gymn.local>
2024-05-08 10:42:47 +0300

    viss varbut gatavs

--- AS_gridasaprekins/Form1.cs
+++ AS_gridasaprekins/Form1.cs
@@ -16,5 +16,43 @@ namespace AS_gridasaprekins
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double telpaG = Convert.ToDouble(telpasg.Text);
+            double telpaP = Convert.ToDouble(telpasp.Text);
+            double laukums1 = telpaG * telpaP;
+            textBox5.Text = Convert.ToString(laukums1 + comboBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double telpaG = Convert.ToDouble(telpasg.Text);
+            double telpaP = Convert.ToDouble(telpasp.Text);
+            double laukums1 = telpaG * telpaP;
+            double Mcena = Convert.ToDouble(textBox3.Text);
+            double izmaksas = laukums1 * Mcena;
+            textBox6.Text = Convert.ToString(izmaksas + "€");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
