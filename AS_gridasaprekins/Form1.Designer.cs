67771cb9df11a7765aba623ed22c21bac981afd0
Adrians Stepe <as3616@gymn.local>
2024-05-15 10:20:57 +0300

    dizains gatavs

--- AS_gridasaprekins/Form1.Designer.cs
+++ AS_gridasaprekins/Form1.Designer.cs
@@ -41,11 +41,17 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // telpasg
             // 
-            this.telpasg.Location = new System.Drawing.Point(152, 164);
+            this.telpasg.Location = new System.Drawing.Point(173, 137);
             this.telpasg.Name = "telpasg";
             this.telpasg.Size = new System.Drawing.Size(100, 20);
             this.telpasg.TabIndex = 0;
@@ -53,15 +59,15 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Location = new System.Drawing.Point(11, 140);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "telpas garums";
+            this.label1.Text = "telpas garums - ";
             // 
             // telpasp
             // 
-            this.telpasp.Location = new System.Drawing.Point(152, 200);
+            this.telpasp.Location = new System.Drawing.Point(173, 173);
             this.telpasp.Name = "telpasp";
             this.telpasp.Size = new System.Drawing.Size(100, 20);
             this.telpasp.TabIndex = 2;
@@ -69,15 +75,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 200);
+            this.label2.Location = new System.Drawing.Point(11, 173);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Size = new System.Drawing.Size(83, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "telpas platums";
+            this.label2.Text = "telpas platums - ";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(152, 238);
+            this.textBox3.Location = new System.Drawing.Point(173, 211);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 4;
@@ -85,7 +91,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 241);
+            this.label3.Location = new System.Drawing.Point(11, 214);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(135, 13);
             this.label3.TabIndex = 5;
@@ -94,16 +100,16 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label5.Location = new System.Drawing.Point(105, 21);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(38, 27);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(593, 31);
+            this.label5.Size = new System.Drawing.Size(706, 37);
             this.label5.TabIndex = 8;
             this.label5.Text = "Grīdas cenas un daudzuma aprēķinu kalkulātors";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(317, 151);
+            this.button1.Location = new System.Drawing.Point(330, 99);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(92, 43);
             this.button1.TabIndex = 9;
@@ -113,7 +119,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(317, 213);
+            this.button2.Location = new System.Drawing.Point(330, 161);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(92, 42);
             this.button2.TabIndex = 10;
@@ -123,14 +129,14 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(446, 164);
+            this.textBox5.Location = new System.Drawing.Point(459, 112);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 11;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(446, 225);
+            this.textBox6.Location = new System.Drawing.Point(459, 173);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 12;
@@ -139,10 +145,10 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "m²",
-            "cm²",
-            "mm²"});
-            this.comboBox1.Location = new System.Drawing.Point(152, 124);
+            "m",
+            "cm",
+            "mm"});
+            this.comboBox1.Location = new System.Drawing.Point(173, 97);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 16;
@@ -151,17 +157,78 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 124);
+            this.label4.Location = new System.Drawing.Point(11, 97);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(118, 13);
             this.label4.TabIndex = 17;
             this.label4.Text = "mērvienība (m, cm, mm)";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Saglabāt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 277);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label6.Location = new System.Drawing.Point(-3, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 15);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "faila nosaukums kuru saglabāt";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(473, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Nolasīt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(451, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Nolasīt rezultātus no faila";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(582, 253);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(162, 175);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox6);
@@ -197,6 +264,12 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- AS_gridasaprekins/Form1.cs
+++ AS_gridasaprekins/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AS_gridasaprekins
 {
@@ -19,10 +20,12 @@ namespace AS_gridasaprekins
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
             double telpaG = Convert.ToDouble(telpasg.Text);
             double telpaP = Convert.ToDouble(telpasp.Text);
             double laukums1 = telpaG * telpaP;
-            textBox5.Text = Convert.ToString(laukums1 + comboBox1.Text);
+            textBox5.Text = Convert.ToString(laukums1 + comboBox1.Text + "²");
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -53,6 +56,43 @@ namespace AS_gridasaprekins
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nosaukums = textBox1.Text.ToString();
+            string teksts = textBox6.Text.ToString();
+            string teksts1 = textBox5.Text.ToString();
+
+            if (File.Exists(nosaukums + ".txt"))
+            {
+                using (StreamWriter sw = File.AppendText(nosaukums + ".txt"))
+                {
+                    sw.WriteLine(teksts);
+                    sw.WriteLine(teksts1);
+                    MessageBox.Show("Fails papildināts!");
+                }
+
+            }
+            else
+            {
+                File.WriteAllText(nosaukums + ".txt", teksts + teksts1 );
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string nosaukums = textBox1.Text.ToString();
+
+            if (File.Exists(nosaukums + ".txt"))
+            {
+                string readText = File.ReadAllText(nosaukums + ".txt");
+                richTextBox1.Text = readText;
+            }
+            else
+            {
+                MessageBox.Show("Fails nav atrasts!");
+            }
         }
     }
 }
